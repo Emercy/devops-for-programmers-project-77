a4c2e2b6735cc5924fadb7637c3372ab9507278e
Emercy <rogoff89@mail.ru>
2023-11-21 23:13:59 +0300

    add tf provider vb

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    virtualbox = {
+      source = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1 "
+    }
     datadog = {
       source = "DataDog/datadog"
     }
